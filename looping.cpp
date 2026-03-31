7427b184b306ca8731cea4d1e8c9e93e8e001873
ahmadmiftianur@gmail.com <ahmadmiftianur@gmail.com>
2026-03-31 21:09:36 +0700

    menambahkan prosedur perulangan engan for

--- looping.cpp
+++ looping.cpp
@@ -2,3 +2,16 @@
 #include <ctime>
 using namespace std;
 
+int main() 
+{
+    int x;
+
+    // Perulangan dengan FOR
+    // mencetak tulisan sebanyak 5 kali
+    cout << "PERULANGAN FOR" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << "Teknologi Informasi UMY" << endl;
+    }
+    cout << endl;
+
+    
