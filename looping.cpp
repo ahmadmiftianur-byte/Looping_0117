faeea8b27b03f275a3c99e7cd04eb54cb4cf8faa
ahmadmiftianur@gmail.com <ahmadmiftianur@gmail.com>
2026-03-31 21:11:22 +0700

    menambahkan prosedur perulangan dengan WHILE untuk mencetak bilangan acak

--- looping.cpp
+++ looping.cpp
@@ -17,4 +17,13 @@ int main()
     // Untuk mendapatkan urutan angka acak yang berbeda
     srand(time(0));
 
+ // Perulangan dengan WHILE
+    cout << "PERULANGAN WHILE" << endl;
+    x = 1 + rand() % 10;
+
+    while (x > 5) {
+        cout << "Bilangan acak = " << x << endl;
+        x = rand() % 10;
+    }
+
     
